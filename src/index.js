6650b1bdfc04c66b73bd4367393eb3b61e53f3c5
ShadReza007 <shadreza3@gmail.com>
2025-03-29 04:07:13 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-app-waaf.onrender.com",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   })
