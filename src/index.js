13a23b4d8022b1d27787dcafe53a4827a254390f
ShadReza007 <shadreza3@gmail.com>
2025-03-29 03:59:05 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-import express from 'express';
-import authRoutes from './routes/auth.route.js';
-import dotenv from 'dotenv';
-import {connectDB} from './lib/db.js';
-import cookieParser from 'cookie-parser';
-import messageRoutes from './routes/message.route.js';
-import cors from 'cors';
-import {app,server} from './lib/socket.js';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT=process.env.PORT;
+const PORT = process.env.PORT;
 
-app.use(express.json({limit:'10mb'}));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin:"https://chat-app-waaf.onrender.com",
-        credentials:true
-    })
+  cors({
+    origin: "https://chat-app-waaf.onrender.com",
+    credentials: true,
+  })
 );
 
-app.use("/api/auth",authRoutes);
-app.use('/api/message',messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-server.listen(PORT,()=>{
-    console.log('Server is running on port : ' + PORT);
-    connectDB();
-})
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
