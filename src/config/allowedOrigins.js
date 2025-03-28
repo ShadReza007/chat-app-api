03ef6176c2bf8d8b376b70566d7a58d741113e8b
ShadReza007 <shadreza3@gmail.com>
2025-03-29 04:31:46 +0530

    Updated

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,11 @@
 const allowedOrigins = [
     'http://localhost:5001',
-    'https://chat-app-waaf.onrender.com'
+    'http://localhost:5173',
+    'http://localhost:5173/api',
+    'https://chat-app-waaf.onrender.com',
+    'https://chat-app-waaf.onrender.com/api',
+    'https://chat-app-api-izbd.onrender.com',
+    'https://chat-app-api-izbd.onrender.com/api',
 ]
 
 export default allowedOrigins;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import corsOptions from "./config/corsOptions.js";
 
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
