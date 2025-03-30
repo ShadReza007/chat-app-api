b13ffd782a13fec08e4aeba661402b861ecb04e3
ShadReza007 <shadreza3@gmail.com>
2025-03-31 00:27:56 +0530

    Updated

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,39 +1,37 @@
-import {Server} from 'socket.io';
-import http from 'http';
-import express from 'express';
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
 
-const app=express();
-const server=http.createServer(app);
+const app = express();
+const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin:["http://localhost:5173"],
-        credentials:true,
-    }
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
 });
 
-export function getReceiverSocketID(userID){
-    return userSocketMap[userID];
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
 }
 
-const userSocketMap={};
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
 
-io.on('connection',(socket)=>{
-    console.log('A User connected',socket.id);
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
 
-    const userID=socket.handshake.query.userID;
-    if(userID){
-        userSocketMap[userID]=socket.id;
-    }
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
 
-    // io.emit() -> send events to all connected clients
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on('disconnect',()=>{
-        console.log('A User disconnected',socket.id);
-        delete userSocketMap[userID];
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    })
-})
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
-export {io,app,server};
+export { io, app, server };
--- src/models/message.model.js
+++ src/models/message.model.js
@@ -1,25 +1,25 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-    senderID:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    receiverID:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    text:{
-        type:String
+    text: {
+      type: String,
     },
-    image:{
-        type:String
-    }
-},{
-    timestamps:true
-  }
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
 const Message = mongoose.model("Message", messageSchema);
